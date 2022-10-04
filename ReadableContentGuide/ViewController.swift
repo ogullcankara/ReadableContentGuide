6b6791a96c9e3437907659eaaa3e7cff82dc6fb6
ogullcankara <ogullcankara@gmail.com>
2022-10-05 00:30:22 +0300

    Initial Commit

---
+++ ReadableContentGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadableContentGuide
+//
+//  Created by Ogulcan Kara on 5.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
